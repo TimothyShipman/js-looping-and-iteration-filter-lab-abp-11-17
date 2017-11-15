7d5828f636564a4cf7dda63b5f794cc47585095c
Tim Shipman <bricklayertim@yahoo.com>
2017-11-15 18:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ function findMatching(drivers, name){
 }
 
 function fuzzyMatch(drivers, firstLetter){
-  let wholeOfName = firstLetter.length;
+  let wholeName = firstLetter.length;
   return drivers.filter(function(driverName){
-    return driverName.slice(0, wholeOfName) === firstLetter;
+    return driverName.slice(0, wholeName) === firstLetter;
   })
 }
 
 function matchName(driver, name){
-  
+  return driver.filter(function(list){
+    return list === name;
+  })
 }
